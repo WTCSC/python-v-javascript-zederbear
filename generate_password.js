4e74d64fc5b64d56120b6898919b4e559b6303f7
Zederbear <hooverzed@gmail.com>
2024-10-09 14:34:17 -0600

    random generate password works

--- generate_password.js
+++ generate_password.js
@@ -1,5 +1,12 @@
 function generatePassword(length = 12) {
-    // complete me
+    let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_"
+
+    let random_password = ""
+    for (let i = 0; i < length; i++) {
+        random_password += characters[Math.floor(Math.random()*characters.length)]
+    }
+    
+    return random_password
 }
 
 module.exports = generatePassword;
